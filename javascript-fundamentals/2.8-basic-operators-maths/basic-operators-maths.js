41aab4514ce66aa9c7e3ed53f38787099d514169
MaxScutum <doncarlos1605@gmail.com>
2026-06-21 02:32:42 +0700

    add lesson 2.8-basic-operators-maths

---
+++ javascript-fundamentals/2.8-basic-operators-maths/basic-operators-maths.js
@@ -0,0 +1,53 @@
+/*
+Addition +,
+Subtraction -,
+Multiplication *,
+Division /,
+Remainder %,
+Exponentiation **.
+*/
+
+// unary
+
+let x = 1;
+
+x = -x;
+alert(x); // -1, unary negation was applied
+
+// binary
+
+let z = 1,
+  y = 3;
+alert(y - z); // 2, binary minus subtracts values
+
+// Remainder %
+
+alert(5 % 2); // 1, the remainder of 5 divided by 2
+alert(8 % 3); // 2, the remainder of 8 divided by 3
+alert(8 % 4); // 0, the remainder of 8 divided by 4
+
+// Exponentiation **
+
+alert(2 ** 2); // 2² = 4
+alert(2 ** 3); // 2³ = 8
+alert(2 ** 4); // 2⁴ = 16
+
+alert(4 ** (1 / 2)); // 2 (power of 1/2 is the same as a square root)
+alert(8 ** (1 / 3)); // 2 (power of 1/3 is the same as a cubic root)
+
+// String concatenation with binary +
+
+let s = "my" + "string";
+alert(s); // mystring
+
+alert("1" + 2); // "12"
+alert(2 + "1"); // "21"
+
+alert(2 + 2 + "1"); // "41" and not "221"
+
+alert("1" + 2 + 2); // "122" and not "14"
+
+alert(6 - "2"); // 4, converts '2' to a number
+alert("6" / "2"); // 3, converts both operands to numbers
+
+// Numeric conversion, unary +

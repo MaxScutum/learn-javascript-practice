87577c94c01540c8d93cea3b3c81c818ff2f5c8a
MaxScutum <doncarlos1605@gmail.com>
2026-06-29 23:48:16 +0700

    add 2.13-loops-while-for

---
+++ javascript-fundamentals/2.13-loops-while-for/loops-while-for.js
@@ -0,0 +1,14 @@
+// 2.13. Loops: while and for.
+
+/*
+Loops are a way to repeat the same code multiple times.
+*/
+
+// The “while” loop
+
+let i = 0;
+while (i < 3) {
+  // shows 0, then 1, then 2
+  alert(i);
+  i++;
+}

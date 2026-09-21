91572ab871f3dd6b046196093e1a9cb572125220
MaxScutum <doncarlos1605@gmail.com>
2026-09-21 23:19:59 +0700

    add interaction-alert-prompt-confirm-task.js

---
+++ 2-javascript-fundamentals/2.6-interaction-alert-prompt-confirm/interaction-alert-prompt-confirm-task.js
@@ -0,0 +1,15 @@
+// 2.6. Interaction: alert, prompt, confirm. Task.
+
+alert("2.6. Interaction. Task.");
+
+// --- Task. A simple page (importance: 4) ---
+/*
+Ask for a name and output it.
+*/
+
+{
+  let userName = prompt("What is your name?", "");
+  alert(userName);
+}
+
+alert("The End of 2.6. Interaction. Task.");
